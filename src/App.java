f97b0ff668eabad37b80a39ef77a3f3e37e0c88f
Ronald <r0nald19202006@gmail.com>
2025-05-21 13:07:32 +0200

    Cambios desde la rama develop

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola, soy Daniel");
         System.out.println("Hola, yo soy Ronald");
+        System.out.println("Hola, estoy en la rama develop");
     }
 }
